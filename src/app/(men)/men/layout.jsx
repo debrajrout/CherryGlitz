85ece1fff41f1e0ba79a05856a16434a5ebcc9b3
Debaraj Rout <debrajrout28@gmail.com>
2024-05-26 05:03:25 +0530

    refactor: Update import paths for global CSS files in men and women layouts

--- src/app/(men)/men/layout.jsx
+++ src/app/(men)/men/layout.jsx
@@ -1,5 +1,5 @@
 import { Inter } from "next/font/google";
-import "../../globals.css";
+import "../globals.css";
 import { Toaster } from "@/components/ui/sonner";
 
 const inter = Inter({ subsets: ["latin"] });
