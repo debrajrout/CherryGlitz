f1a1f4db522fd582b04fa25bf5cb2350c82e66d0
Debaraj Rout <debrajrout28@gmail.com>
2024-08-11 10:59:06 +0530

    refactor: Update import paths for global CSS files in men and women layouts

--- src/components/auth/login-button.jsx
+++ src/components/auth/login-button.jsx
@@ -1,5 +1,4 @@
-import { Copy } from "lucide-react"
-
+"use client"
 import { Button } from "@/components/ui/button"
 import {
     Dialog,
@@ -11,8 +10,6 @@ import {
     DialogTitle,
     DialogTrigger,
 } from "@/components/ui/dialog"
-import { Input } from "@/components/ui/input"
-import { Label } from "@/components/ui/label"
 import { login } from "@/actions/authaction"
 
 export function DialogCloseButton() {
